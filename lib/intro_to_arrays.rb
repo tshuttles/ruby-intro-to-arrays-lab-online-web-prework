9e73989f53a182cd4fab2e92d08fae0de6bfb92e
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-11 01:28:06 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  @my_new_array = []
+end 
+
+def array_with_two_elements
+  two_elements = [1, 2]
+end
+
+def first_element(array)
+  array = ["Welcome to New York", 2]
+  array[0]
+end
+
+def third_element(array)
+  array = [1, 2, "Style"]
+  array[2]
+end
+
+def last_element(array)
+  array = [1, 2, "Out of The Woods"]
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array = ["Thailand", 2, 3]
+  array.first
+end 
+
+def last_element_with_array_methods(array)
+  array = [1, 2, "Myanmar"]
+  array.last
+end
+
+def length_of_array(length)
+  array = 8
+end
